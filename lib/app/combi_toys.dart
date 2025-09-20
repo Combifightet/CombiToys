4ae4ae6e55e82cd6d80f400d88f7fd7ec73d0378
Combifightet <malte@hei-sharp-s.de>
2025-09-20 19:15:53 +0200

    [Feat] added localisation overhead (for `en` and `de`)

--- lib/app/combi_toys.dart
+++ lib/app/combi_toys.dart
@@ -1,16 +1,37 @@
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-import '/app/home_page.dart';
+import 'package:combi_toys/l10n/generated/app_localizations.dart';
+import 'package:combi_toys/app/home_page.dart';
 
 class CombiToys extends StatelessWidget {
   const CombiToys({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CombiToys',
-      home: HomePage(),
+    return ChangeNotifierProvider(
+      create: (context) => LocaleModel(),
+      child: Consumer<LocaleModel>(
+        builder: (context, localeModel, child) => MaterialApp(
+          title: 'CombiToys',
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          locale: localeModel.locale,
+          home: HomePage(),
+        )
+      ),
     );
+  }
+}
+
+class LocaleModel extends ChangeNotifier {
+  Locale? _locale;
+
+  Locale? get locale => _locale;
+
+  void set(Locale locale) {
+    _locale = locale;
+    notifyListeners();
   }
 }
--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,5 +1,9 @@
 
+import 'package:combi_toys/app/combi_toys.dart';
 import 'package:flutter/material.dart';
+
+import 'package:combi_toys/l10n/generated/app_localizations.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,22 +13,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Locale? _selectedLocale;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedLocale = Provider.of<LocaleModel>(context, listen: false).locale;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xfffcfbfa),
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text(
+          AppLocalizations.of(context)!.homePage
+        ),
         backgroundColor: Color(0xff2c2b2a),
         foregroundColor: Color(0xfffcfbfa),
       ),
       body: Center(
-        child: Text(
-          '<missing content>',
-          style: TextStyle(
-            fontSize: 18,
-            color: Color(0xff2c2b2a),
-          ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  AppLocalizations.of(context)!.language,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xff2c2b2a),
+                  ),                  
+                ),
+                DropdownButton(
+                  value: _selectedLocale,
+                  items: AppLocalizations.supportedLocales.map(
+                    (locale) => DropdownMenuItem(
+                      value: locale,
+                      child: Text(locale.toString())
+                    )
+                  ).toList(),
+                  onChanged: (Locale? locale) {
+                    if (locale!=null) {
+                      _selectedLocale = locale;
+                      Provider.of<LocaleModel>(context, listen: false).set(locale);
+                    }
+                  }
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
