ea934098603529a84cb544ec89544b569a9f4cc3
Combifightet <malte@hei-sharp-s.de>
2025-10-16 09:26:02 +0200

    feat: adding more widget samples

--- lib/app/widgets_page.dart
+++ lib/app/widgets_page.dart
@@ -31,7 +31,7 @@ class _WidgetsPageState extends State<WidgetsPage> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 SizedBox(height: 16,),
-                Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque bibendum est mauris, eu malesuada est ultricies eget. In hac habitasse platea dictumst. Fusce a lacinia mi, ut efficitur orci. Aliquam erat volutpat.'),
+                Text('This Page is only used to preview the global Theme on most flutter widgets.'),
                 SizedBox(height: 32,),
                 Expanded(
                   child: SingleChildScrollView(
@@ -54,8 +54,7 @@ class _WidgetsPageState extends State<WidgetsPage> {
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
                                   Column(
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
+                                    children: [
                                       ElevatedButton(onPressed: (() {}), child: const Text('Elevated')),
                                       SizedBox(height: 8,),
                                       FilledButton(onPressed: (() {}), child: const Text('Filled')),
@@ -69,8 +68,7 @@ class _WidgetsPageState extends State<WidgetsPage> {
                                   ),
                                   SizedBox(width: 16,),
                                   Column(
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                    children: <Widget>[
+                                    children: [
                                       ElevatedButton(onPressed: null, child: const Text('Elevated')),
                                       SizedBox(height: 8,),
                                       FilledButton(onPressed: null, child: const Text('Filled')),
@@ -87,21 +85,173 @@ class _WidgetsPageState extends State<WidgetsPage> {
                             ),
                             ShowcaseItem(
                               heading: 'DropdownButton',
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  DropdownButton(
+                                    value: 1,
+                                    icon: const Icon(Icons.arrow_downward),
+                                    items: [
+                                      DropdownMenuItem(value: 1, child: Text('Item 1')),
+                                      DropdownMenuItem(value: 2, child: Text('Item 2')),
+                                      DropdownMenuItem(value: 3, child: Text('Item 3')),
+                                    ],
+                                    onChanged: (_) {}
+                                  ),
+                                  SizedBox(height: 8,),
+                                  DropdownButton(
+                                    value: 1,
+                                    icon: const Icon(Icons.arrow_downward),
+                                    items: [
+                                      DropdownMenuItem(value: 1, child: Text('Item 1')),
+                                    ],
+                                    onChanged: null
+                                  ),
+                                ],
+                              ),
                             ),
                             ShowcaseItem(
                               heading: 'DropdownMenu',
-                            ),
-                            ShowcaseItem(
-                              heading: 'ExtendedFloatingActionButton',
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  DropdownMenu(
+                                    initialSelection: 1,
+                                    enableFilter: true,
+                                    dropdownMenuEntries: [
+                                      DropdownMenuEntry(value: 1, label: 'Item 1'),
+                                      DropdownMenuEntry(value: 2, label: 'Item 2'),
+                                      DropdownMenuEntry(value: 3, label: 'Item 3'),
+                                    ],
+                                  ),
+                                  SizedBox(height: 8,),
+                                  DropdownMenu(
+                                    enabled: false,
+                                    initialSelection: 1,
+                                    dropdownMenuEntries: [
+                                      DropdownMenuEntry(value: 1, label: 'Item 1'),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                             ShowcaseItem(
                               heading: 'FloatingActionButton',
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                children: [
+                                  Column(
+                                    children: [
+                                      Text('Small'),
+                                      SizedBox(height: 8,),
+                                      Text('Regular'),
+                                      SizedBox(height: 8,),
+                                      Text('Large'),
+                                      SizedBox(height: 8,),
+                                      Text('Extended'),
+                                    ],
+                                  ),
+                                  SizedBox(width: 16,),
+                                  Column(
+                                    children: [
+                                      FloatingActionButton.small(onPressed: () {}, child: const Icon(Icons.check)),
+                                      SizedBox(height: 8,),
+                                      FloatingActionButton(onPressed: () {}, child: const Icon(Icons.check)),
+                                      SizedBox(height: 8,),
+                                      FloatingActionButton.large(onPressed: () {}, child: const Icon(Icons.check)),
+                                      SizedBox(height: 8,),
+                                      FloatingActionButton.extended(onPressed: () {}, label: Text('OK'), icon: const Icon(Icons.check)),
+                                    ],
+                                  ),
+                                  SizedBox(width: 16,),
+                                  Column(
+                                    children: [
+                                      FloatingActionButton.small(onPressed: null, child: const Icon(Icons.check)),
+                                      SizedBox(height: 8,),
+                                      FloatingActionButton(onPressed: null, child: const Icon(Icons.check)),
+                                      SizedBox(height: 8,),
+                                      FloatingActionButton.large(onPressed: null, child: const Icon(Icons.check)),
+                                      SizedBox(height: 8,),
+                                      FloatingActionButton.extended(onPressed: null, label: Text('OK'), icon: const Icon(Icons.check)),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                             ShowcaseItem(
                               heading: 'IconButton',
+                              child: Row(
+                                children: [
+                                  Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text('Normal'),
+                                      SizedBox(height: 8,),
+                                      Text('Filled'),
+                                      SizedBox(height: 8,),
+                                      Text('FilledTonal'),
+                                      SizedBox(height: 8,),
+                                      Text('Outlined'),
+                                      SizedBox(height: 8,),
+                                    ],
+                                  ),
+                                  SizedBox(width: 16,),
+                                  Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(onPressed: () {}, icon: const Icon(Icons.add)),
+                                      SizedBox(height: 8,),
+                                      IconButton.filled(onPressed: () {}, icon: const Icon(Icons.add)),
+                                      SizedBox(height: 8,),
+                                      IconButton.filledTonal(onPressed: () {}, icon: const Icon(Icons.add)),
+                                      SizedBox(height: 8,),
+                                      IconButton.outlined(onPressed: () {}, icon: const Icon(Icons.add)),
+                                      SizedBox(height: 8,),
+                                    ],
+                                  ),
+                                  SizedBox(width: 16,),
+                                  Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(onPressed: null, icon: const Icon(Icons.add)),
+                                      SizedBox(height: 8,),
+                                      IconButton.filled(onPressed: null, icon: const Icon(Icons.add)),
+                                      SizedBox(height: 8,),
+                                      IconButton.filledTonal(onPressed: null, icon: const Icon(Icons.add)),
+                                      SizedBox(height: 8,),
+                                      IconButton.outlined(onPressed: null, icon: const Icon(Icons.add)),
+                                      SizedBox(height: 8,),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                             ShowcaseItem(
                               heading: 'PopupMenuButton',
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  PopupMenuButton(
+                                    initialValue: 1,
+                                    onSelected: null,
+                                    itemBuilder: (_) => [
+                                      const PopupMenuItem(value: 1, child: Text('Item 1')),
+                                      const PopupMenuItem(value: 2, child: Text('Item 2')),
+                                      const PopupMenuItem(value: 3, child: Text('Item 3')),
+                                    ],
+                                  ),
+                                  SizedBox(height: 8,),
+                                  PopupMenuButton(
+                                    enabled: false,
+                                    initialValue: 1,
+                                    onSelected: null,
+                                    itemBuilder: (_) => [
+                                      const PopupMenuItem(value: 1, child: Text('Item 1')),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                             ShowcaseItem(
                               heading: 'RefreshIndicator',
